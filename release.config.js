954ebcf669cae6087ea447fc484460363b1aecdc
Mike Lima <mike.rodrigues.lima@gmail.com>
2023-08-08 22:17:01 -0300

    feat: add new version

--- release.config.js
+++ release.config.js
@@ -1,19 +1,24 @@
 const config = {
-  branches: ["teste"],
+  branches: ['teste'],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/git", {
-      "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
-    "@semantic-release/github"
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['dist/**/*.{js,css}', 'docs', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github'
     // ["semantic-release-ado", {
     //   "varName": "version",
     //   "setOnlyOnRelease": true,
     //   "isOutput": true
     // }],
   ]
-}
+};
 
-module.exports = config
+module.exports = config;
