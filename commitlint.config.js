c3bbf78aa8d211773280121b83e634f147112973
Mike Lima <mike.rodrigues.lima@gmail.com>
2023-08-09 23:28:20 -0300

    chore: commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   ignores: [(message) => message.includes('release')],
   rules: {
-    "scope-enum": [2, "always", [
+    "scope-enum": [2, "never", [
       "logging",
       "services",
       "docs",
