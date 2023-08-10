26dacbfa69c8365106b8392eda4114f607e2594e
Mike Lima <mike.rodrigues.lima@gmail.com>
2023-08-09 23:16:21 -0300

    chore: change commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,22 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   ignores: [(message) => message.includes('release')],
   rules: {
-    'scope-enum': [
-      2,
-      'always',
-      [
-        'ui-components',
-        'ui-components/badge',
-        'ui-components/button',
-        'ui-components/tooltip',
-        'core',
-        'account',
-        'plugins',
-        'settings',
-        'projects',
-        'shared',
-        'styles'
-      ]
-    ]
+    'scope-enum': [2, 'always', "ola"]
   }
 };
