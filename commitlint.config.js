261edd4b4678f9fa9146ad85f5241169233bfcda
Mike Lima <mike.rodrigues.lima@gmail.com>
2023-08-09 23:25:36 -0300

    refactor: change commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,17 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   ignores: [(message) => message.includes('release')],
   rules: {
-    'scope-enum': [2, 'always', "ola"]
+    "scope-enum": [2, "always", [
+      "logging",
+      "services",
+      "docs",
+      "dependencies",
+      "profiles",
+      "users",
+      "api",
+      "segments",
+      "configuration"
+    ]],
+    "subject-empty": [2, "never"],
   }
 };
