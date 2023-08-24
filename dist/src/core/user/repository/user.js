1e54d09fe0622cede010d62b35150a4ed0896c0d
Mike Lima <mike.rodrigues.lima@gmail.com>
2023-08-23 22:54:27 -0300

    chore(remove): remove dist folder

--- dist/src/core/user/repository/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IUserRepository = void 0;
-const repository_1 = require("../../../infra/repository");
-class IUserRepository extends repository_1.IRepository {
-}
-exports.IUserRepository = IUserRepository;
-//# sourceMappingURL=user.js.map
