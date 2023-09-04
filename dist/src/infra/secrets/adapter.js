07bec639c303caf48f03d20c7e402fafb80a41ad
Mike Lima <mike.rodrigues.lima@gmail.com>
2023-09-03 22:43:04 -0300

    chore(remove): delete dist folder

--- dist/src/infra/secrets/adapter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ISecretsAdapter = void 0;
-class ISecretsAdapter {
-}
-exports.ISecretsAdapter = ISecretsAdapter;
-//# sourceMappingURL=adapter.js.map
--- dist/src/modules/logout/adapter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ILogoutAdapter = void 0;
-class ILogoutAdapter {
-}
-exports.ILogoutAdapter = ILogoutAdapter;
-//# sourceMappingURL=adapter.js.map
