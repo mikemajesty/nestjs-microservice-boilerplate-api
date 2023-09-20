0a30e48b7bc67fed39e28291f1f2ea588288b698
Mike Rodrigues De Lima <mike.rodrigues.lima@gmail.com>
2023-09-19 23:02:21 -0300

    Delete dist directory

--- dist/src/infra/logger/adapter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ILoggerAdapter = void 0;
-class ILoggerAdapter {
-}
-exports.ILoggerAdapter = ILoggerAdapter;
-//# sourceMappingURL=adapter.js.map
--- dist/src/libs/auth/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_1 = require("../../core/user/entity/user");
-const Schema = user_1.UserEntitySchema.pick({
-    login: true,
-    password: true,
-    roles: true
-});
-//# sourceMappingURL=types.js.map
