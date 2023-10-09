71b13020f0414a4275b7aaee8661baf1a8ae4490
Mike Rodrigues De Lima <mike.rodrigues.lima@gmail.com>
2023-10-08 21:25:08 -0300

    Delete dist directory

--- dist/src/core/cats/repository/cats.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ICatsRepository = void 0;
-const repository_1 = require("../../../infra/repository");
-class ICatsRepository extends repository_1.IRepository {
-}
-exports.ICatsRepository = ICatsRepository;
-//# sourceMappingURL=cats.js.map
