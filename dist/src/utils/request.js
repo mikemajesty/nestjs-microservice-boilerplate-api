07a439a153eaaf990126cc014a379c3fcfd262a2
Mike Rodrigues De Lima <mike.rodrigues.lima@gmail.com>
2023-11-15 23:06:08 -0300

    Delete dist directory

--- dist/src/utils/request.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPathWithoutUUID = void 0;
-const getPathWithoutUUID = (path) => path.replace(/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/, 'uuid');
-exports.getPathWithoutUUID = getPathWithoutUUID;
-//# sourceMappingURL=request.js.map
