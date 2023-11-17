939f728a7c3313dc1f163bffbeed7eefe94ca3ea
Mike Lima <mike.rodrigues.lima@gmail.com>
2023-11-17 14:53:51 -0300

    chore(config): rename containers

--- scripts/mongo/start-replicaset.sh
+++ scripts/mongo/start-replicaset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker-compose -f docker-compose-infra.yml down
-docker-compose -f docker-compose-infra.yml up -d 
+docker-compose -f docker-compose-infra.yml up -d --remove-orphans
 
 sleep 30
 
