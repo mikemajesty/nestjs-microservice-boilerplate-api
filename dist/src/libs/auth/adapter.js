8834cffb216a6eba649c242256dab0e053590bcb
Mike Rodrigues De Lima <mike.rodrigues.lima@gmail.com>
2023-11-19 01:12:27 -0300

    Delete dist directory

--- dist/src/libs/auth/adapter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ITokenAdapter = void 0;
-class ITokenAdapter {
-}
-exports.ITokenAdapter = ITokenAdapter;
-//# sourceMappingURL=adapter.js.map
