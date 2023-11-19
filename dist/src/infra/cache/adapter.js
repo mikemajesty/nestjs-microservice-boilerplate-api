d5347ef6bee8f3b734a49af27d38440a90c135f2
Mike Rodrigues De Lima <mike.rodrigues.lima@gmail.com>
2023-11-18 23:44:01 -0300

    Delete dist directory

--- dist/src/infra/cache/adapter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ICacheAdapter = void 0;
-class ICacheAdapter {
-}
-exports.ICacheAdapter = ICacheAdapter;
-//# sourceMappingURL=adapter.js.map
