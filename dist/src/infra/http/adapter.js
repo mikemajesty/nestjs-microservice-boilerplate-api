2b0d43134c53699e5e3092bf023cea6940d3695c
Mike Rodrigues De Lima <mike.rodrigues.lima@gmail.com>
2023-11-29 14:00:11 -0300

    Delete dist directory

--- dist/src/infra/http/adapter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IHttpAdapter = void 0;
-class IHttpAdapter {
-}
-exports.IHttpAdapter = IHttpAdapter;
-//# sourceMappingURL=adapter.js.map
