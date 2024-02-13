1d9df291741ee513f38b540f50a7f98ad532bb4e
mike-lima-gaivota <mike.lima@gaivota.ai>
2024-02-13 16:00:00 -0300

    chore(config): remove changelog

--- scripts/generate-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-NODE_VERSION=$(node -p -e "require('./package.json').version")
-./node_modules/generate-changelog/bin/generate -t $NODE_VERSION
-git add package.json
-git add package-lock.json
-git add CHANGELOG.md
-git commit -m "feat(release): generate version: $NODE_VERSION" -n
