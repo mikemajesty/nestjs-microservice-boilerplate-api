96f599b8550102cbd42d9eaf47c5c83bb1b83323
mike-lima-gaivota <mike.lima@gaivota.ai>
2024-04-16 20:43:59 -0300

    chore(i18n): add translate generic type

--- src/libs/i18n/service.ts
+++ src/libs/i18n/service.ts
@@ -8,8 +8,8 @@ import { TranslateOptions } from './types';
 export class I18nService implements II18nAdapter {
   constructor(private readonly i18n: Service) {}
 
-  translate(key: string, options: TranslateOptions = {}): unknown {
+  translate<T = unknown>(key: string, options: TranslateOptions = {}): T {
     Object.assign(options, { lang: I18nContext.current().lang });
-    return this.i18n.translate(key, options);
+    return this.i18n.translate(key, options) as T;
   }
 }
