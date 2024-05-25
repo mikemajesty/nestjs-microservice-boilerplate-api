926cb789883b795ee2ee53ca3b64ab549d9f25ca
Mike Lima <mike.rodrigues.lima@gmail.com>
2024-05-25 15:13:43 -0300

    chore(secrets): remove literal env string

--- src/infra/secrets/service.ts
+++ src/infra/secrets/service.ts
@@ -8,9 +8,9 @@ import { EnvEnum } from './types';
 export class SecretsService implements ISecretsAdapter {
   constructor(private readonly config: ConfigService) {}
 
-  IS_LOCAL = this.config.get<string>('NODE_ENV') === 'local';
+  IS_LOCAL = this.config.get<EnvEnum>('NODE_ENV') === EnvEnum.LOCAL;
 
-  IS_PRODUCTION = this.config.get<string>('NODE_ENV') === 'prod';
+  IS_PRODUCTION = this.config.get<EnvEnum>('NODE_ENV') === EnvEnum.PRD;
 
   ENV = this.config.get<EnvEnum>('NODE_ENV');
 
