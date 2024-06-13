fcd6af4a33b1d5a60b9a4d35477daae33fa99d9f
Mike Lima <mike.rodrigues.lima@gmail.com>
2024-06-13 16:58:11 -0300

    chore(config): add permission name validator

--- src/utils/decorators/role.decorator.ts
+++ src/utils/decorators/role.decorator.ts
@@ -1,4 +1,14 @@
 import { SetMetadata } from '@nestjs/common';
 
+import { ApiInternalServerException } from '../exception';
+
 export const PERMISSION_KEY = 'permissions';
-export const Permission = (...roles: string[]) => SetMetadata(PERMISSION_KEY, roles);
+export const Permission = (permission: string) => {
+  const permissionSanitize = permission.toLowerCase();
+
+  if (!permissionSanitize.includes(':')) {
+    throw new ApiInternalServerException(`permission: ${permission} must contain ":", example: user:create`);
+  }
+
+  return SetMetadata(PERMISSION_KEY, permissionSanitize);
+};
