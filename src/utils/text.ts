1132987652842f9eb3d1d294200a794f9672072d
mike-lima-gaivota <mike.lima@gaivota.ai>
2024-10-12 23:51:48 -0300

    feat(utils): add text utils

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -19,4 +19,30 @@ export class TextUtils {
           .trim()
       : text;
   };
+
+  static formatCapitalizeText = (text: string, matchLength = 4) => {
+    const textList = text.split(' ');
+
+    const NO_SPACE = 1;
+
+    if (textList.length === NO_SPACE) {
+      if (text.length >= matchLength) {
+        return TextUtils.capitalizeFirstLetter(text);
+      }
+
+      return text;
+    }
+
+    const fullText = textList.reduce((accumulator, currentValue) => {
+      if (accumulator.length >= matchLength) {
+        accumulator = `${accumulator.charAt(0).toUpperCase()}${accumulator.slice(1)}`;
+      }
+      if (currentValue.length >= matchLength) {
+        currentValue = `${currentValue.charAt(0).toUpperCase()}${currentValue.slice(1)}`;
+      }
+      return `${accumulator} ${currentValue}`.trim();
+    });
+
+    return fullText;
+  };
 }
