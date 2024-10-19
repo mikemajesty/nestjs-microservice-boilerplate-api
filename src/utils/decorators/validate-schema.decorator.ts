a2a988c0ec9f1ee2e9b9ca24af7beca69f92be9b
mike-lima-gaivota <mike.lima@gaivota.ai>
2024-10-19 14:03:46 -0300

    chore(config): add try handler on zod validator

--- src/utils/decorators/validate-schema.decorator.ts
+++ src/utils/decorators/validate-schema.decorator.ts
@@ -4,9 +4,13 @@ export function ValidateSchema(...schema: Schema[]) {
   return (target: unknown, propertyKey: string, descriptor: PropertyDescriptor) => {
     const originalMethod = descriptor.value;
     descriptor.value = function (...args: unknown[]) {
-      for (const [index, value] of schema.entries()) {
-        const model = value.parse(args[`${index}`]);
-        args[`${index}`] = model;
+      try {
+        for (const [index, value] of schema.entries()) {
+          const model = value.parse(args[`${index}`]);
+          args[`${index}`] = model;
+        }
+      } catch (error) {
+        throw error;
       }
       const result = originalMethod.apply(this, args);
       return result;
