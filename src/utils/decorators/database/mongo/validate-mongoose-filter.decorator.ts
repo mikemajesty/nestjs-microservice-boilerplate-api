bec245f42eadedf1464c7dfa54da7b07b0fad052
mike-lima-gaivota <mike.lima@gaivota.ai>
2024-10-30 13:27:25 -0300

    fix(decorators): fix mongoose or filter

--- src/utils/decorators/database/mongo/validate-mongoose-filter.decorator.ts
+++ src/utils/decorators/database/mongo/validate-mongoose-filter.decorator.ts
@@ -35,7 +35,12 @@ const convertFilterValue = (input: Pick<AllowedFilter<unknown>, 'format'> & { va
   }
 
   if (input.format === 'ObjectId') {
-    return new Types.ObjectId(`${input.value}`);
+    const isObjectId = MongoUtils.isObjectId(`${input.value}`);
+
+    if (!isObjectId) {
+      throw new ApiBadRequestException('invalid objectId');
+    }
+    return new Types.ObjectId(`${input.value} `);
   }
 
   return input.value;
@@ -44,16 +49,17 @@ const convertFilterValue = (input: Pick<AllowedFilter<unknown>, 'format'> & { va
 export function ConvertMongooseFilter<T>(allowedFilterList: AllowedFilter<T>[] = []) {
   return function (target: unknown, propertyKey: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
-    descriptor.value = function (...args: { search: { [key: string]: string } }[]) {
+    descriptor.value = function (...args: { search: { [key: string]: string | string[] } }[]) {
       const input = args[0];
 
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const where: any = {};
 
       where['deletedAt'] = null;
+      where['$or'] = [];
 
       if (input?.search?.id) {
-        where['_id'] = input.search.id.trim();
+        where['_id'] = (input.search.id as string).trim();
         delete input.search.id;
       }
 
@@ -61,7 +67,7 @@ export function ConvertMongooseFilter<T>(allowedFilterList: AllowedFilter<T>[] =
 
       Object.keys(input.search || {}).forEach((key) => {
         const allowed = filterNameList.includes(key);
-        if (!allowed) throw new ApiBadRequestException(`allowed filters are: ${filterNameList.join(', ')}`);
+        if (!allowed) throw new ApiBadRequestException(`allowed filters are: ${filterNameList.join(', ')} `);
       });
 
       for (const allowedFilter of allowedFilterList) {
@@ -74,43 +80,56 @@ export function ConvertMongooseFilter<T>(allowedFilterList: AllowedFilter<T>[] =
 
         if (allowedFilter.type === SearchTypeEnum.equal) {
           if (typeof regexFilter === 'object') {
-            where['$or'] = regexFilter.map((filter) => {
-              return {
-                [`${allowedFilter.map ?? (allowedFilter.name as string)}`]: convertFilterValue({
-                  value: filter,
-                  format: allowedFilter.format
-                })
-              };
-            });
+            (where['$or'] as unknown[]).push(
+              ...(filter as string[]).map((filter) => {
+                return {
+                  [`${allowedFilter.map ?? (allowedFilter.name as string)} `]: convertFilterValue({
+                    value: filter,
+                    format: allowedFilter.format
+                  })
+                };
+              })
+            );
           }
           if (typeof regexFilter === 'string') {
-            where[`${allowedFilter.map ?? (allowedFilter.name as string)}`] = convertFilterValue({
-              value: filter,
-              format: allowedFilter.format
+            (where['$or'] as unknown[]).push({
+              [`${allowedFilter.map ?? (allowedFilter.name as string)} `]: convertFilterValue({
+                value: filter,
+                format: allowedFilter.format
+              })
             });
           }
         }
 
         if (allowedFilter.type === SearchTypeEnum.like) {
           if (typeof regexFilter === 'object') {
-            where['$or'] = regexFilter.map((filter) => {
-              return {
-                [`${allowedFilter.map ?? (allowedFilter.name as string)}`]: {
-                  $regex: filter,
-                  $options: 'i'
-                }
-              };
-            });
+            (where['$or'] as unknown[]).push(
+              ...regexFilter.map((filter) => {
+                return {
+                  [`${allowedFilter.map ?? (allowedFilter.name as string)} `]: {
+                    $regex: filter,
+                    $options: 'i'
+                  }
+                };
+              })
+            );
           }
 
           if (typeof regexFilter === 'string') {
-            where[`${allowedFilter.map ?? (allowedFilter.name as string)}`] = {
-              $regex: MongoUtils.createMongoRegexText(filter),
-              $options: 'i'
-            };
+            (where['$or'] as unknown[]).push({
+              [`${allowedFilter.map ?? (allowedFilter.name as string)} `]: {
+                $regex: MongoUtils.createMongoRegexText(filter),
+                $options: 'i'
+              }
+            });
           }
         }
       }
+
+      if (!where['$or'].length) {
+        delete where['$or'];
+      }
+
       args[0].search = where;
       const result = originalMethod.apply(this, args);
       return result;
