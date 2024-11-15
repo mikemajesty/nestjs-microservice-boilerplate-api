d00bfa4461523007c47d617434275eee4682e318
mike-lima-gaivota <mike.lima@gaivota.ai>
2024-11-14 21:58:09 -0300

    chore(utils): make search required

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export type SearchInput<T> = { search?: T | null };
+export type SearchInput<T> = { search: T | null };
 
 export const SearchHttpSchema = z
   .string()
@@ -51,5 +51,8 @@ export const SearchHttpSchema = z
 export type SearchHttpSchemaInput = z.infer<typeof SearchHttpSchema>;
 
 export const SearchSchema = z.object({
-  search: z.record(z.string().trim(), z.number().or(z.string()).or(z.array(z.any()))).nullable()
+  search: z
+    .record(z.string().trim(), z.number().or(z.string()).or(z.array(z.any())))
+    .nullable()
+    .default({})
 });
