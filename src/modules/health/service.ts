05eb17f1ba417d74eeb980d9be0f8771566db478
Mike Lima <mike.rodrigues.lima@gmail.com>
2025-03-02 20:44:05 -0300

    refactor(health): fix getActiveConnections func

--- src/modules/health/service.ts
+++ src/modules/health/service.ts
@@ -99,7 +99,7 @@ export class HealthService implements IHealthAdapter {
 
   getActiveConnections() {
     return new Promise((resolve, reject) => {
-      exec('lsof -i -n | grep ESTABLISHED | wc -l', (error, stdout, stderr) => {
+      exec(`lsof -i -n -p ${process.pid} | grep ESTABLISHED | wc -l`, (error, stdout, stderr) => {
         if (error) {
           reject(`Error getting connection: ${stderr}`);
         }
