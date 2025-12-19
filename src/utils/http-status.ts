f836579017cf4e4bf252a1393191977125f003cd
Mike Lima <mike.lima@aolxbr.com>
2025-12-19 01:17:58 -0300

    ci(none): try to fix ci action

--- src/utils/http-status.ts
+++ src/utils/http-status.ts
@@ -3,20 +3,22 @@
  */
 export const DefaultErrorMessage: { [key: string]: string } = {
   ECONNREFUSED: 'Connection Refused',
+  401: "These aren't the droids you're looking for",
   403: 'You Shall Not Pass',
+  404: 'The truth is out there',
   405: 'Method Not Allowed',
   406: 'Not Acceptable',
-  408: 'Request Timeout',
+  408: 'Time is a flat circle',
   413: 'Payload Too Large',
   414: 'URI Too Long',
   422: 'Unprocessable Entity',
   428: 'Precondition Required',
   429: 'Too Many Requests',
-  500: 'Internal Server Error.',
+  500: 'Houston, we have a problem.',
   501: 'Not Implemented',
-  502: 'Bad Gateway',
-  503: 'Service Unavailable',
+  502: 'The cake is a lie',
+  503: 'Winter is coming',
   504: 'Gateway Timeout',
   507: 'Insufficient Storage',
-  508: 'Loop Detected'
+  508: "I've got déjà vu again"
 }
